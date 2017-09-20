9b0242b70ee21c1e90a421d8a4d7304b1218d0c7
Tao Yang <yangtao.now@gmail.com>
2017-09-20 11:22:19 +0800

    set default fdt_file to imx6ul-14x14-gateway.dtb

--- include/configs/mx6ul_14x14_evk.h
+++ include/configs/mx6ul_14x14_evk.h
@@ -136,7 +136,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=undefined\0" \
+	"fdt_file=imx6ul-14x14-gateway.dtb\0" \
 	"fdt_addr=0x83000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
